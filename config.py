1d566239c241a86aec5fd7d6e12dae6d3118391f
Himanshu Chotaliya <himanshu746h@gmail.com>
2026-05-28 17:42:18 +0530

    V2 Crisismitra

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
+
 class Config:
-    SECRET_KEY='HIMANSHU'
-    SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
-    JWT_SECRET_KEY='HIMANSHU'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'HIMANSHU')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'HIMANSHU')
     JWT_TOKEN_LOCATION = ['cookies']
     JWT_COOKIE_CSRF_PROTECT = False
